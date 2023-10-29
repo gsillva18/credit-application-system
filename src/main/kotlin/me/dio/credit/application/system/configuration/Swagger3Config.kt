503848e6d0e5d52cb1afed03bdb3ede1c58fc13d
José Gabriel da Silva Lima <josegabrielg831@gmail.com>
2023-10-29 17:26:09 -0300

    Update OpenAi configuration class

--- src/main/kotlin/me/dio/credit/application/system/configuration/Swagger3Config.kt
+++ src/main/kotlin/me/dio/credit/application/system/configuration/Swagger3Config.kt
@@ -1,5 +1,9 @@
 package me.dio.credit.application.system.configuration
 
+import io.swagger.v3.core.model.ApiDescription
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,4 +17,14 @@ class Swagger3Config {
       .pathsToMatch("/api/customers/**", "/api/credits/**")
       .build()
   }
+
+
+  @Bean
+  fun api(): OpenAPI {
+    return OpenAPI().info(Info()
+                    .title("Application customers and credits ")
+                    .description("Application where customers can request credits")
+                    .version("v1.0.0"))
+  }
+
 }
